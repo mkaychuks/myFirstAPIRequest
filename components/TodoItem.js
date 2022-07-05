ee61c9110edb1cbc81810d5036d9f34d9f7f905f
mkaychuks <sachufusi@gmail.com>
2022-07-05 14:24:34 +0100

    Minor fixes

---
+++ components/TodoItem.js
@@ -0,0 +1,32 @@
+import { StyleSheet, View } from "react-native";
+import React from "react";
+import { List } from "react-native-paper";
+
+const TodoItem = (props) => {
+  return (
+    <View style={styles.listItem}>
+      <List.Item
+        title={props.title}
+        left={() => <List.Icon color={props.color} icon={props.icon} />}
+        titleStyle={styles.title}
+      />
+    </View>
+  );
+};
+
+export default TodoItem;
+
+const styles = StyleSheet.create({
+  listItem: {
+    width: "100%",
+    borderColor: "#ccc",
+    borderWidth: 1,
+    borderRadius: 4,
+    marginBottom: 8,
+    flex: 1,
+  },
+  title: {
+    fontWeight: "700",
+    paddingEnd: 12,
+  },
+});
