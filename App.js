95adad54b5f8beb0928cb0a231721d91a8f74b44
mkaychuks <sachufusi@gmail.com>
2022-07-06 13:30:31 +0100

    Wrapped up :happy:

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
-import { Provider as PaperProvider, Avatar, List } from "react-native-paper";
-import { StyleSheet, View, FlatList, Text } from "react-native";
+import { Provider as PaperProvider, Avatar } from "react-native-paper";
+import { StyleSheet, View, FlatList, Text, Pressable } from "react-native";
 import React, { useState, useEffect } from "react";
 
 import TodoItem from "./components/TodoItem";
@@ -18,10 +18,14 @@ export default function App() {
   }, []);
 
   const deleteTodo = (id) => {
-    setTodos(currentTodo => {
-      return currentTodo.filter((todo) => todo.id !== id )
-    })
-  }
+    setTodos((currentTodo) => {
+      return currentTodo.filter((todo) => todo.id !== id);
+    });
+  };
+
+  const deleteAllTodo = () => {
+    setTodos([]);
+  };
 
   return (
     <PaperProvider>
@@ -33,7 +37,22 @@ export default function App() {
           </View>
         ) : (
           <View>
-            <Text style={styles.headerTitle}>Todos</Text>
+            <Text style={styles.headerTitle}>
+              Todos
+              <Pressable onPress={deleteAllTodo} style={{ marginLeft: 15 }}>
+                <Text
+                  style={{
+                    color: "blue",
+                    opacity: 0.3,
+                    fontStyle: "italic",
+                    textTransform: "lowercase",
+                    fontSize: 12,
+                  }}
+                >
+                  Empty List
+                </Text>
+              </Pressable>
+            </Text>
             <FlatList
               alwaysBounceVertical={false}
               showsVerticalScrollIndicator={false}
@@ -44,8 +63,8 @@ export default function App() {
                   title={item.title}
                   icon={item.id % 2 === 0 ? "calendar" : "folder"}
                   color={item.id % 2 === 0 ? "red" : "blue"}
-                  onDelete = {deleteTodo}
-                  id = {item.id}
+                  onDelete={deleteTodo}
+                  id={item.id}
                 />
               )}
             />
@@ -71,8 +90,8 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   headerTitle: {
-    fontWeight: '700',
+    fontWeight: "700",
     fontSize: 22,
-    margin: 8
-  }
+    margin: 8,
+  },
 });
